18a7347e3b98f69ab38fd0388908c812c4173e5f
Aberran <anaberrantis@gmail.com>
2023-12-17 18:38:20 +0100

    selenium remove

--- conftest.py
+++ conftest.py
@@ -1,10 +1,6 @@
 import pytest
-import time
 from pytest_factoryboy import register
 from tests.factories import UserFactory
-from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 register(UserFactory)
 
@@ -13,18 +9,7 @@ def new_user1(db, user_factory):
     user = user_factory.create()
     return user
 
-@pytest.fixture(scope="class")
-def chrome_driver_init(request):
     
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--window-size=800,600")
-    service = Service(ChromeDriverManager().install())
-    chrome_driver = webdriver.Chrome(service=service, options=options)
-    request.cls.driver = chrome_driver
-    yield
-    time.sleep(2)
-    chrome_driver.quit()
     
 
     
