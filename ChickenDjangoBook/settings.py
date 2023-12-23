69264b3c028f3dfe9addf2974cd77d0adb564a68
Aberran <anaberrantis@gmail.com>
2023-12-23 18:09:29 +0100

    uprava

--- ChickenDjangoBook/settings.py
+++ ChickenDjangoBook/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.213']
 
 
 # Application definition
