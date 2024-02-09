19a7cb5d25bfa2a9cd8fb286b37c57d695388ba4
Jiří Trnečka <trnecka.jiri@gmail.com>
2024-02-09 23:51:12 +0100

    Inserted documentation for ChickenBook classes and methods

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,20 @@ import os
 
 
 class UserManager(BaseUserManager):
+    """
+    Class is used for creating superuser in terminal
+    """
     def create_user(self, email, password, first_name=None, last_name=None, **extra_fields):
+        """
+        Creating user
+
+        Args:
+            email (_type_): User's email
+            password (_type_): User's password
+            first_name (_type_, optional): The first name of the user, it must be inserted.
+            last_name (_type_, optional): The last name of the user, it must be inserted.
+
+        """
         if not email:
             raise ValueError('Enter an email address')
         if not first_name:
@@ -23,6 +36,16 @@ class UserManager(BaseUserManager):
         return user
     
     def create_superuser(self, email, password, first_name, last_name):
+        """
+        Creating super user
+
+        Args:
+            email (str): The email of the super user
+            password (str): The password of the super user
+            first_name (str): The first name of the super user
+            last_name (str): The last name of the super user
+
+        """
         user = self.create_user(email=email, password=password, first_name=first_name, last_name=last_name)
         user.is_superuser = True
         user.is_staff = True
@@ -30,6 +53,9 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractUser):
+    """
+    The class represented the user in the database
+    """
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -48,6 +74,10 @@ class User(AbstractUser):
  
     # IMG SHRINKING
     def save(self, *args, **kwargs):
+        """
+        Saving the class model to the database
+        """
+        
         
         super().save(*args, **kwargs)
         
@@ -65,7 +95,9 @@ class User(AbstractUser):
     objects = UserManager()        
  
 class Skills(models.Model):
-
+    """
+    The class represented the skills in the database
+    """
     name = models.CharField(max_length=200, blank=True, null=True)
     level = models.PositiveSmallIntegerField(choices=(
         (1, "★☆☆"),
@@ -78,7 +110,9 @@ class Skills(models.Model):
         return self.name
     
 class Project(models.Model):
-    
+    """
+    The class represented the projects in the database
+    """    
     project_name=models.CharField(max_length=20, blank=True, null=True)
     project_link=models.URLField(max_length=200, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
--- accounts/utils.py
+++ accounts/utils.py
@@ -3,6 +3,9 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.contrib.auth.base_user import AbstractBaseUser
 
 class AppTokenGenerator(PasswordResetTokenGenerator):
+    """
+    Generate token for ChickenBook API
+    """
     def _make_hash_value(self, user: AbstractBaseUser, timestamp: int) -> str:
         return (f"{user.is_active}{user.pk}{timestamp}")
 
